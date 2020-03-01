eef5e25b967fb54ba76c55c13a42622006ab8536
NikhilBansal02 <52885161+NikhilBansal02@users.noreply.github.com>
2020-03-01 21:52:18 +0530

    Add files via upload

---
+++ MultipleMain.java
@@ -0,0 +1,19 @@
+//We can have multiple main method with different types of arguments
+//if no main method is present with string args[], then 
+//class will compile successfully but run time error will be thrown.
+package concepts;
+
+public class MultipleMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to main with String arguments");
+		int a[] = {1,2,3};
+		main(a);
+
+	}
+	public static void main(int[] args) {
+		System.out.println("Welcome to main with int arguments");
+		
+	}
+
+}
